c724063d78a5cf230957af8e89f0834306510914
Eldar <eldarzr@post.bgu.ac.il>
2021-10-22 13:36:45 +0300

    add files

---
+++ HelloWorld.cpp
@@ -0,0 +1,11 @@
+﻿// HelloWorld.cpp : Defines the entry point for the application.
+//
+
+#include "HelloWorld.h"
+
+using namespace std;
+
+void printHelloWorld() {
+	std::cout << "hello world" << std::endl;
+}
+
---
+++ HelloWorld.h
@@ -0,0 +1,10 @@
+﻿// HelloWorld.h : Include file for standard system include files,
+// or project specific include files.
+
+#pragma once
+
+#include <iostream>
+
+void printHelloWorld();
+
+// TODO: Reference additional headers your program requires here.
